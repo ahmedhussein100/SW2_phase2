95aa211e93c36bab18f46729d2c875ee81d9d73f
ahmedhussein100 <ahmedhussein0048@gmail.com>
2020-06-09 19:48:32 +0200

    mobile class
    add mobile classs attributes and constructor

---
+++ lib/mobile_class.dart
@@ -0,0 +1,20 @@
+class Mobiles{
+  String Mobile_Name;
+  String Mobile_storage;
+  String Mobile_ram;
+  String Mobile_Quantity;
+  String Mobile_price;
+  String Mobile_Location;
+  String Mobile_frontCamera;
+  String Mobile_backCamera;
+
+  Mobiles({this.Mobile_Name,
+    this.Mobile_storage,
+    this.Mobile_ram,
+    this.Mobile_Quantity,
+    this.Mobile_price,
+    this.Mobile_Location,
+    this.Mobile_backCamera,
+    this.Mobile_frontCamera
+  });
+}
